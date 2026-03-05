588d913ec8480e158bd8cf8d84146d9950f7649f
julian <julianpedraza19378@gmail.com>
2026-03-04 22:54:57 -0500

    new head

--- frontend-app/src/App.vue
+++ frontend-app/src/App.vue
@@ -2,7 +2,7 @@
   <div class="app">
     <header>
       <h1>Sistema de Pedidos</h1>
-      <span class="env-badge">DEVELOPER -v</span>
+      <span class="env-badge">DEVELOPER 1</span>
     </header>
 
     <main>
